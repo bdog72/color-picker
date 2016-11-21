e2f3adfe8cc2ac736da36796455033cfc39481bc
Brian Bycynski <high5bri@hotmail.com>
2016-11-21 15:43:12 -0500

    Work complete

--- public/main.js
+++ public/main.js
@@ -1,6 +1,39 @@
+let hue = 0
+let sat = 100
+let light = 50
+
+// const color = { hue: 0, saturation: 100, lightness: 50 }
+
+const getHSL = () => {
+  return `hsl(${hue}, ${sat}%, ${light}%)`
+}
+const getSAT = () => {
+  return `hsl(${hue}, ${sat}%, ${light}%)`
+}
+const getLIGHT = () => {
+  return `hsl(${hue}, ${sat}%, ${light}%)`
+}
+
 const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+  document.addEventListener
+
+  const hueInput = document.getElementById('hu')
+  hueInput.addEventListener('input', () => {
+    hue = hueInput.value
+    document.querySelector('.sec1').style.backgroundColor = getHSL()
+  })
+
+  const satInput = document.getElementById('sat')
+  satInput.addEventListener('input', () => {
+    sat = satInput.value
+    document.querySelector('.sec1').style.backgroundColor = getSAT()
+  })
+
+  const lightInput = document.getElementById('light')
+  lightInput.addEventListener('input', () => {
+    light = lightInput.value
+    document.querySelector('.sec1').style.backgroundColor = getLIGHT()
+  })
 }
 
 document.addEventListener('DOMContentLoaded', main)
-
